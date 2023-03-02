7a6f39bb3e861d25f386b8d1bd794efdb3cd227c
qrach <125902082+qrach@users.noreply.github.com>
2023-03-02 01:05:18 +0000

    Update server.js

--- server.js
+++ server.js
@@ -1,24 +1,10 @@
-const http = require('http');
-const httpProxy = require('http-proxy');
+const http = require('https');
 
-const proxy = httpProxy.createProxyServer({});
-
-const server = http.createServer((req, res) => {
-  const urienc = req.url.slice(6); // slice '/prox?' from the url
-  if (!urienc) {
-    res.writeHead(400, { 'Content-Type': 'text/plain' });
-    res.end('Missing "urienc" parameter');
-    return;
-  }
-  const uri = Buffer.from(urienc, 'base64').toString();
-  proxy.web(req, res, { target: uri }, err => {
-    console.log('Proxy error:', err);
-    res.writeHead(500, { 'Content-Type': 'text/plain' });
-    res.end('Proxy error');
+http.createServer((req, res) => {
+  const targetUrl = 'https://google.com'; // Replace with the target URL
+  const targetReq = http.request(targetUrl, targetRes => {
+    res.writeHead(targetRes.statusCode, targetRes.headers);
+    targetRes.pipe(res);
   });
-});
-
-const port = process.env.PORT || 5000;
-server.listen(port, () => {
-  console.log(`Proxy server listening on port ${port}`);
-});
+  req.pipe(targetReq);
+}).listen(8080); // Replace with your desired port
